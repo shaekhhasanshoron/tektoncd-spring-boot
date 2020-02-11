aed78091ce067bd4a26d7f579ab3993b062dec94
zerosmi <www.zeromsi2@gmail.com>
2020-02-11 12:37:58 +0600

    Update dockerfile and setting.xml, add script that replaces nexus-public urls, username and pass

--- script_to_set_pomfile.sh
+++ script_to_set_pomfile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i 's=</project>=<distributionManagement><repository><id>nexus</id><name>Releases</name><url>'"$NEXUS_RELEASES_URL"'</url></repository><snapshotRepository><id>nexus</id><name>Snapshot</name><url>'"$NEXUS_SNAPSHOTS_URL"'</url></snapshotRepository></distributionManagement>=g'  pom.xml
+sed -i 's=</project>=<distributionManagement><repository><id>nexus</id><name>Releases</name><url>'"$NEXUS_RELEASES_URL"'</url></repository><snapshotRepository><id>nexus</id><name>Snapshot</name><url>'"$NEXUS_SNAPSHOTS_URL"'</url></snapshotRepository></distributionManagement> </project>=g'  pom.xml
 
 cat pom.xml
 
